41929ee3d76bea82bc85a14bf0eff7b4ad4716fb
carlos13suarez <charliesuarez10@gmail.com>
2024-09-10 18:41:55 -0500

    task 1

---
+++ debugging/factorial.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
+if len(sys.argv) != 2:
+    print("Usage: python3 factorial.py <number>")
+    sys.exit(1)
+
+try:
+    number = int(sys.argv[1])
+    if number < 0:
+        raise ValueError("Factorial is not defined for negative numbers.")
+except ValueError as e:
+    print(f"Invalid input: {e}")
+    sys.exit(1)
+
+f = factorial(number)
+print(f)
